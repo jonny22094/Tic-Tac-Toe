48d8d00488a330e7843bb9a4ad1e1ae9253db3e8
Jan Rochalski <qwsazxxzaswq@wp.pl>
2018-03-21 21:21:11 +0100

    Update

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require( 'express' );
+const express = require("express");
 const app     = express();
-const http    = require( 'http' ).Server( app );
-const io      = require( 'socket.io' )( http );
+const http    = require("http").Server(app);
+const io      = require("socket.io")(http);
 
-const rooms = {};
+let rooms = {};
 const lines = [
     [0, 1, 2],
     [3, 4, 5],
@@ -15,93 +15,83 @@ const lines = [
     [2, 4, 6],
 ];
 
-app.use( express.static( __dirname + '/client' ) );
+app.use(express.static(`${__dirname}/client`));
 
-io.on( 'connection', ( socket ) => {
-    const user = {
-        room: null,
-        round: null
+io.on("connection", user => {
+  let roomId = null;
+
+  for(const room in rooms) {
+    if(rooms[room].players.length === 1) {
+      rooms[room].players.push(user);
+      rooms[room].ready = true;
+
+      roomId = room;
+      sendData(roomId, "alert", "Game started");
     }
+  }
 
-    for( const id in rooms ){
-        if ( rooms[ id ].userTwo === null ) {
-            rooms[ id ].userTwo = socket;
-            rooms[ id ].start   = true;
+  if(!roomId) {
+    roomId = createRoom(user);
+  }
 
-            user.room = id;
-            user.round = 1;
+  user.on("move", data => {
+    let room = rooms[roomId];
+    if(room.ready && user.id === room.players[room.turn].id && room.maps[data] === null) {
+      room.maps[data] = room.turn;
+      room.moves++;
 
-                         socket.emit( "alert", "Find opponent" );
-            rooms[ id ].userOne.emit( "alert", "Find opponent" );
+      sendData(roomId, "move", {id: data, text: room.turn ? "X" : "O"});
+      room.turn = room.turn ? 0 : 1;
 
-                         socket.emit( "alert", "Opponent start" );
-            rooms[ id ].userOne.emit( "alert", "You start" );
-        }
+      if(checkMap(room)) {
+        sendData(roomId, "alert", `Win ${room.turn ? "X" : "O"}`);
+        room.ready = false;
+      }
+      else if(room.moves === 9) {
+        sendData(roomId, "alert", "Draw");
+        room.ready = false;
+      }
+
+      rooms[roomId] = room;
     }
+  });
 
-    if ( user.room === null ) {
-        const newRoom = {};
-
-        newRoom.userOne = socket;
-        newRoom.userTwo = null;
-        newRoom.map     = new Array( 9 ).fill( null );
-        newRoom.round   = 0;
-        newRoom.moves   = 0;
-        newRoom.start   = false;
-
-        rooms[ socket.id ] = newRoom;
-
-        user.room = socket.id;
-        user.round = 0;
-
-        socket.emit( "alert", "Waiting for oponent" );
-    }
-
-    socket.on( "move", data => {
-        if ( rooms[ user.room ] != null && user.round === rooms[ user.room ].round && rooms[ user.room ].map[ data ] === null && rooms[ user.room ].start ) {
-                rooms[ user.room ].map[ data ] = user.round;
-                rooms[ user.room ].moves++;
-
-                rooms[ user.room ].userOne.emit( "move", { id: data, text: rooms[ user.room ].round ? "X" : "O" } );
-                rooms[ user.room ].userTwo.emit( "move", { id: data, text: rooms[ user.room ].round ? "X" : "O" } );
-
-                for( let i = 0; i < lines.length; i++ ) {
-                    const [ a, b, c ] = lines[ i ];
-
-                    if ( rooms[ user.room ].map[ a ] !== null
-                      && rooms[ user.room ].map[ a ] === rooms[ user.room ].map[ b ]
-                      && rooms[ user.room ].map[ a ] === rooms[ user.room ].map[ c ] ) {
-                          rooms[ user.room ].start = false;
-
-                          socket.emit( "alert", "You win" );
-
-                          if ( rooms[ socket.id ] == null ) rooms[ user.room ].userOne.emit( "alert", "You lose" );
-                          else                              rooms[ socket.id ].userTwo.emit( "alert", "You lose" );
-                    }
-                }
-
-                if ( rooms[ user.room ].moves === 9 ) {
-                    rooms[ user.room ].start = false;
-
-                    rooms[ user.room ].userOne.emit( "alert", "Draw" );
-                    rooms[ user.room ].userTwo.emit( "alert", "Draw" );
-                }
-
-                rooms[ user.room ].round = rooms[ user.room ].round ? 0 : 1;
-        }
-    } )
-
-    socket.once( "disconnect", () => {
-
-        if( rooms[ user.room ] != null ) {
-                 if ( rooms[ user.room ].userOne != null ) rooms[ user.room ].userOne.emit( "alert", "You oponent disconnected" );
-            else if ( rooms[ user.room ].userTwo != null ) rooms[ user.room ].userTwo.emit( "alert", "You oponent disconnected" );
-        }
-
-        delete rooms[ user.room ];
-    } );
-
+  user.once("disconnect", () => {
+    rooms[roomId].ready = false;
+    sendData(roomId, "alert", "Oponent disconnected");
+  });
 });
+
+const sendData = (roomId, event, data) => {
+  rooms[roomId].players.map(user => {
+    user.emit(event, data);
+  });
+}
+
+const createRoom = data => {
+  const newRoom = {
+    players: [data],
+    maps: new Array(9).fill(null),
+    turn: 0,
+    moves: 0,
+    ready: false
+  };
+  rooms[data.id] = newRoom;
+  sendData(data.id, "alert", "Waiting for oponent...");
+
+  return data.id;
+}
+
+const checkMap = room => {
+  for(let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i] ;
+    if(room.maps[a] !== null && room.maps[a] === room.maps[b] && room.maps[a] === room.maps[c]) {
+      return true;
+    }
+  }
+
+  return false;
+}
 
 http.listen( 3000, () => {
   console.log('server start in 3000 port');
